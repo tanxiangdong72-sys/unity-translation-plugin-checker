1b48f31ff7b2832370fb6dce2047bba30361e964
tanxiangdong72-sys <tanxiangdong72@gmail.com>
2026-09-01 13:39:07 +0800

    docs: explain audit rule responsibilities

--- Packages/com.yxwm.localization-auditor/Editor/Scanning/IAuditRule.cs
+++ Packages/com.yxwm.localization-auditor/Editor/Scanning/IAuditRule.cs
@@ -3,6 +3,7 @@ using System.Threading;
 
 namespace Yxwm.LocalizationAuditor
 {
+    // 每条规则只负责产生自己的问题，执行顺序和结果汇总由 AuditRunner 统一管理。
     public interface IAuditRule
     {
         string Id { get; }
